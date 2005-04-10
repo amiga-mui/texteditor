9d051bcec4bf72d1471b01ba929fa07e6fd06825
Jens Maus <mail@jens-maus.de>
2005-04-10 08:23:48 +0000

    - includes/mccclass_68k: replaced the assembler based OS4/68k stub by a easier   manageable C-based variant.

--- mcp/rev.h
+++ mcp/rev.h
@@ -21,9 +21,9 @@
 ***************************************************************************/
 
 #define LIB_VERSION    15
-#define LIB_REVISION   5
+#define LIB_REVISION   6
 
-#define LIB_REV_STRING "15.5"
+#define LIB_REV_STRING "15.6"
 #define LIB_DATE       "09.04.2005"
 
 #if defined(__PPC__)
