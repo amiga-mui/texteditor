ce9756260b8769512d2408c92cbc69fee862a780
David Rey <tactica@users.sf.net>
2005-04-11 03:04:38 +0000

    - Updated guide, fixed copyright notices, corrected sample text in prefs   program

--- mcp/rev.h
+++ mcp/rev.h
@@ -44,4 +44,4 @@
   #define CPU ""
 #endif
 
-#define LIB_COPYRIGHT  "Copyright (c) 1997-2005 TextEditor.mcc Open Source Team"
+#define LIB_COPYRIGHT  "Copyright (C) 2005 TextEditor.mcc Open Source Team"
