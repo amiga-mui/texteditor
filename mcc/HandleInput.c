f7d6f2bb8a951ca10492eaa47f0bf74add370e7b
Jens Maus <mail@jens-maus.de>
2005-06-12 08:20:20 +0000

    - mcc/HandleInput.c: applied a patch from Gunther Nikl which removes the   forbidden floating point math in the wheelmouse scrolling code to due   our shared library nature.

--- mcc/HandleInput.c
+++ mcc/HandleInput.c
@@ -134,8 +134,8 @@ ULONG HandleInput(struct IClass *cl, Object *obj, struct MUIP_HandleEvent *msg)
               get(obj, MUIA_TextEditor_Prop_Visible, &visible);
 		    			if(visible > 0)
 				    	{
-						    // we scroll 1/6 of the displayed text by default
-    						LONG delta = ((((double)visible)/6.0)+0.5); // round the value
+						    // we scroll about 1/6 of the displayed text by default
+    						LONG delta = (visible + 3) / 6;
 
 		    				// make sure that we scroll at least 1 line
 				    		if(delta < 1) delta = 1;
