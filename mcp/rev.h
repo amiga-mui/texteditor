7ece25653610516565c898bee74b958d8e6624d6
Jens Maus <mail@jens-maus.de>
2007-02-25 17:23:40 +0000

    - final version 15.20 release preparation

--- mcp/rev.h
+++ mcp/rev.h
@@ -21,9 +21,9 @@
 ***************************************************************************/
 
 #define LIB_VERSION    15
-#define LIB_REVISION   19
+#define LIB_REVISION   20
 
-#define LIB_REV_STRING "15.19"
+#define LIB_REV_STRING "15.20"
 #define LIB_DATE       "25.02.2007"
 
 #if defined(__PPC__)
