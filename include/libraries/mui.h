4e348fc63fc85bd613b3f731e16bb89bbcb4c8e0
Thore Böckelmann <tboeckel@gmx.de>
2010-04-27 08:52:47 +0000

    * include/libraries/mui.h: removed custom raw image tags of Bitmap.mui.

--- include/libraries/mui.h
+++ include/libraries/mui.h
@@ -1874,21 +1874,12 @@ extern char MUIC_Bitmap[];
 #define MUIA_Bitmap_Height                  0x80421560 /* V8  isg LONG              */
 #define MUIA_Bitmap_MappingTable            0x8042e23d /* V8  isg UBYTE *           */
 #define MUIA_Bitmap_Precision               0x80420c74 /* V11 isg LONG              */
-#if defined(__amigaos4__)
-#define MUIA_Bitmap_RawData                 0x8042f61a /* V20 isg APTR              */
-#define MUIA_Bitmap_RawDataFormat           0x80429b85 /* V20 isg ULONG             */
-#endif
 #define MUIA_Bitmap_RemappedBitmap          0x80423a47 /* V11 ..g struct BitMap *   */
 #define MUIA_Bitmap_SourceColors            0x80425360 /* V8  isg ULONG *           */
 #define MUIA_Bitmap_Transparent             0x80422805 /* V8  isg LONG              */
 #define MUIA_Bitmap_UseFriend               0x804239d8 /* V11 i.. BOOL              */
 #define MUIA_Bitmap_Width                   0x8042eb3a /* V8  isg LONG              */
 
-#if defined(__amigaos4__)
-#define MUIV_Bitmap_RawDataFormat_CLUT8 0
-#define MUIV_Bitmap_RawDataFormat_RGB24 1
-#define MUIV_Bitmap_RawDataFormat_ARGB32 2
-#endif
 
 
 /****************************************************************************/
