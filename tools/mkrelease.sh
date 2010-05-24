cfae2f8b9bbdaa75b60bd86d8b05a1c93887bbd5
Mariusz Danilewicz <mandi@interia.pl>
2010-05-24 09:37:05 +0000

    * mcp\locale\polish.ct: added Polish translation  * tools\mkrelease.sh: added generating Polish catalog to release  * mcp\makefile and mmakefile.src: added generating Polish catalog

--- tools/mkrelease.sh
+++ tools/mkrelease.sh
@@ -63,7 +63,7 @@ for os in os3 os4 mos aros-i386 aros-ppc aros-x86_64; do
 done
 
 make -C mcp catalogs
-for language in czech danish french german russian swedish; do
+for language in czech danish french german russian swedish polish; do
 	mkdir -p "release/MCC_TextEditor/Locale/Catalogs/$language"
 	cp -a mcp/locale/$language.catalog "release/MCC_TextEditor/Locale/Catalogs/$language/TextEditor_mcp.catalog"
 done
