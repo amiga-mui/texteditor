2b6b91b8081d4041dcfec7baea2e9fb36e230942
Thore Böckelmann <tboeckel@gmx.de>
2013-11-01 18:47:32 +0000

    * mcc/SpellChecker.c, mcc/GetSetAttrs.c: changed the MUIA_TextEditor_Keywords attribute to take a comma separated list instead of an array.

--- include/mui/TextEditor_mcc.h
+++ include/mui/TextEditor_mcc.h
@@ -106,7 +106,7 @@ extern "C" {
 #define MUIA_TextEditor_WrapWords             (TextEditor_Dummy + 0x3e)
 #define MUIA_TextEditor_TabSize               (TextEditor_Dummy + 0x3f)
 #define MUIA_TextEditor_Keywords              (TextEditor_Dummy + 0x40)
-#define MUIA_TextEditor_KeywordFound          (TextEditor_Dummy + 0x41)
+#define MUIA_TextEditor_MatchedKeyword        (TextEditor_Dummy + 0x41)
 
 #define MUIM_TextEditor_HandleError           (TextEditor_Dummy + 0x1f)
 #define MUIM_TextEditor_AddKeyBindings        (TextEditor_Dummy + 0x22)
