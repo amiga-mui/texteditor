626065197337484ab6226b3e1deb15a75d400a9a
Thore Böckelmann <tboeckel@gmx.de>
2016-08-15 11:42:56 +0200

    * all/Debug.c: declare inlined _INDENT() function as static to avoid warnings of GCC5.

--- mcp/Debug.c
+++ mcp/Debug.c
@@ -254,7 +254,7 @@ void CleanupDebug(void)
 
 /****************************************************************************/
 
-INLINE void _INDENT(void)
+STATIC INLINE void _INDENT(void)
 {
   int i;
   for(i=0; i < indent_level; i++)
