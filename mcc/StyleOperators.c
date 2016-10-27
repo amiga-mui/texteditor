62e38fb4f2d6cfcc005b12d3a7696abaf58b2d13
alpyre <amithlondestek@gmail.com>
2016-10-27 10:39:30 +0300

    Code style preservations.

--- mcc/StyleOperators.c
+++ mcc/StyleOperators.c
@@ -234,7 +234,7 @@ void AddStyle(struct InstData *data, struct marking *realblock, UWORD style, BOO
       return;
     }
   }
-  data->HasChanged  = TRUE;
+  data->HasChanged = TRUE;
   data->ChangeEvent = TRUE;
 
   if(realblock->enabled == TRUE && (realblock->startx != realblock->stopx || realblock->startline != realblock->stopline))
