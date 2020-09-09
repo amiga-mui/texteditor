836d3de7977722b31f9c3de141c560ce4976e63f
Thore Böckelmann <tboeckel@gmx.de>
2020-09-09 07:20:00 +0200

    * ClipboardServer.c: changed the default path for "CURRDIR:" and "PROGDIR:" to "T:", because the "MUI:" assign does not exist on all systems, i.e. AROS. This closes #31.

--- mcc/ClipboardServer.c
+++ mcc/ClipboardServer.c
@@ -960,8 +960,8 @@ BOOL StartClipboardServer(void)
                                       #elif defined(__MORPHOS__)
                                       NP_CodeType, CODETYPE_PPC,
                                       #endif
-                                      NP_CurrentDir, Lock("MUI:", SHARED_LOCK),
-                                      NP_ProgramDir, Lock("MUI:", SHARED_LOCK),
+                                      NP_CurrentDir, Lock("T:", SHARED_LOCK),
+                                      NP_ProgramDir, Lock("T:", SHARED_LOCK),
                                       TAG_DONE);
     if(serverProcess !=  NULL)
     {
